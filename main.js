b5189599fbca256174f42daecb94e07f295c2256
Ilsa. <ilsasyeda@gmail.com>
2024-03-16 14:57:43 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+var alien_color = "Red";
+//if statement
+if (alien_color == "Red") {
+    console.log("you shot an alien\nearned 5 points!");
+}
+else {
+    //else statement
+    console.log("shoot the alien..");
+}
+;
---
+++ main.ts
@@ -0,0 +1,9 @@
+var alien_color: string = "Red";
+
+//if statement
+if(alien_color == "Red"){
+    console.log("you shot an alien\nearned 5 points!")
+}else{
+//else statement
+    console.log("shoot the alien..")
+};
